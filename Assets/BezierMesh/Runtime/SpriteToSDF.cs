f2771db8a636738f726203f0eb89c425f5fd3b40
sugino <git@sugi.cc>
2021-07-28 02:56:31 +0900

    texture quad の位置合わせ

--- Assets/BezierMesh/Runtime/SpriteToSDF.cs
+++ Assets/BezierMesh/Runtime/SpriteToSDF.cs
@@ -9,12 +9,23 @@ public class SpriteToSDF : MonoBehaviour
     [SerializeField] Texture2D texture;
     [SerializeField] Material drawMat;
     [SerializeField] bool expandEdges;
+    [SerializeField] Renderer quad;
 
     public void GenerateSDF(Sprite source)
     {
-        var bounds = source.bounds;
+        var rect = source.rect;
         if (texture != null)
             DestroyImmediate(texture);
-        texture = VectorUtils.RenderSpriteToTexture2D(source, Mathf.RoundToInt(bounds.size.x * 64), Mathf.RoundToInt(bounds.size.y * 64), drawMat, 1, expandEdges);
+        texture = VectorUtils.RenderSpriteToTexture2D(source, Mathf.RoundToInt(rect.width* 64), Mathf.RoundToInt(rect.width * 64), drawMat, 1, expandEdges);
+        texture.wrapMode = TextureWrapMode.Clamp;
+        var mpb = new MaterialPropertyBlock();
+        if (quad != null)
+        {
+            quad.GetPropertyBlock(mpb);
+            mpb.SetTexture("_MainTex", texture);
+            quad.SetPropertyBlock(mpb);
+            quad.transform.localPosition = rect.center;
+            quad.transform.localScale = new Vector3(rect.size.x, rect.size.y, 1f);
+        }
     }
 }
